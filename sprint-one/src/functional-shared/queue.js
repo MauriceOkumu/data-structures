642c19084afb30c1ec21f6a337cf37788f346959
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-12-14 12:05:15 -0800

    Functional shared all specs passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.first = 0;
+  someInstance.last = 0;
+  someInstance = _.extend(someInstance,queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function (val) {
+	this.storage[++this.last] = val;
+
+};
+
+queueMethods.dequeue = function () {
+  if (this.last - this.first > 0) {
+  	var temp = this.storage[++this.first];
+  }
+  return temp;
+};
+
+queueMethods.size = function () {
+  return this.last - this.first;
+};
 
 
