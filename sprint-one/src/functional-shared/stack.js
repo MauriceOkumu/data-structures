037029db61b40a89882b71da9531b1b17dc7869d
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-12-14 11:47:57 -0800

    Functional shared size spec passing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+    //storage.size = 0;
+    someInstance.storage = {};
+    someInstance.numKeys = 0;
+    someInstance = _.extend(someInstance,stackMethods);
+    return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (val) {
+  this.storage[++this.numKeys] = val;
+};
 
+stackMethods.pop = function () {
+
+};
+
+stackMethods.size = function () {
+ return this.numKeys;
+};
