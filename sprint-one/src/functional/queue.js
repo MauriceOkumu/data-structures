9ffbac83083081b372f4d24200c5e14285faff28
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-12-14 11:28:46 -0800

    Functional Queue specs passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var firstIn = 0;
+  var lastIn = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[++lastIn] = value;
   };
 
   someInstance.dequeue = function() {
+    if( lastIn - firstIn > 0) {
+      var val = storage[++firstIn];
+    }
+    return val;
   };
 
   someInstance.size = function() {
+    return lastIn - firstIn;
   };
 
   return someInstance;
