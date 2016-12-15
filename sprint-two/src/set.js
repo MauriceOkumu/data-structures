b4eeb172fb1c8d9a9566d951676f448513522361
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-12-14 19:17:27 -0800

    Sets done

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (item in this._storage) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  Constant time lookup O(n);
  */
