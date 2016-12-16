1c17879d14e964f9f70095e180531f6c1035a513
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-12-15 23:30:37 -0800

    Binary Search tree all tests passing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,58 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(binaryMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
 
+var binaryMethods = {};
+
+binaryMethods.insert = function(value) {
+  var descend = function (node) {
+    if (node.value > value) {
+      if (node.left === null) {
+        node.left = BinarySearchTree(value);
+      } else {
+        descend(node.left);
+      }
+    } else if (node.value < value) {
+      if (node.right === null) {
+        node.right = BinarySearchTree(value);
+      } else {
+        descend(node.right);
+      }
+    }
+  };
+  descend(this);
+};
+
+binaryMethods.contains = function(target) {
+  var descend = function (node) {
+    if (node && node.value === target) {
+      return true;
+    }
+    if (node.left && node.value > target) {
+      return descend(node.left);
+    } else if (node.right && node.value < target) {
+      return descend(node.right);
+    }
+    return false;
+  };
+  return descend(this);
+};
+
+binaryMethods.depthFirstLog = function(cb) {
+  var descend = function (node) {
+    cb(node.value);
+    if (node.left) {
+      descend(node.left);
+    } else if (node.right) {
+      descend(node.right);
+    }
+  };
+  descend(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
